0fe2645028f20ff57d4a6cab402ddc2b9b05ac27
Mike Degatano <michael.degatano@gmail.com>
2024-01-08 10:25:01 -0500

    Fix decorator tests

--- tests/jobs/test_job_decorator.py
+++ tests/jobs/test_job_decorator.py
@@ -990,6 +990,7 @@ async def test_internal_jobs_no_notify(coresys: CoreSys):
                     "stage": None,
                     "done": True,
                     "parent_id": None,
+                    "errors": [],
                 },
             },
         }
